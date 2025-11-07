189d0e465a8c23f40f4b1cbfa9cdead6292fda77
alexgriffin-gli <alex.griffin@golocalinteractive.com>
2025-11-06 20:21:02 -0600

    Delete EVChargeTracker/EVChargeTracker/Sources/EVChargeTracker directory

--- EVChargeTracker/EVChargeTracker/Sources/EVChargeTracker/Extensions/Color+Extensions.swift
+++
@@ -1,5 +0,0 @@
-import SwiftUI
-
-extension Color {
-    static let deepForestGreen = Color(red: 0.0, green: 0.5, blue: 0.0)
-}
--- EVChargeTracker/EVChargeTracker/Sources/EVChargeTracker/Views/Components/CustomDivider.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct CustomDivider: View {
-    var body: some View {
-        Rectangle()
-            .fill(Color.gray.opacity(0.5))
-            .frame(height: 1)
-    }
-}
